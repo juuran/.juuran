c179e18541f7af13b73d320d9bc6c234b96709c1
Juuso Välimaa <juuso.valimaa@kela.fi>
2023-05-11 14:55:28 +0300

    Embettering the pull-all
    Clearer logs. Added help. Made prettier and fixed bug that emerged from changing the branch conditionally.

--- omat/.config/omat/skriptit/git-pull-all.sh
+++ omat/.config/omat/skriptit/git-pull-all.sh
@@ -11,38 +11,49 @@ parallel="false"
 onlyFetch="false"
 runCustomCommand="false"
 doToAll=""
+showHelp=false
 
 for arg in "$@"; do
-  [[ $arg == "parallel" ]] || [[ $arg == "paral" ]] && parallel="true"
-  [[ $arg == "fetch" ]] && onlyFetch="true" && echo -e "Performing 'fetch --all --prune' on all projects."
-  [[ $arg == "do" ]] && doToAll="$2" && runCustomCommand="true" && onlyFetch="false"
+  [ "$arg" == "parallel" ] || [ "$arg" == "paral" ] && parallel="true"
+  [ "$arg" == "fetch" ] && onlyFetch="true" && echo -e "Performing 'fetch --all --prune' on all projects."
+  [ "$arg" == "do" ] && doToAll="$2" && runCustomCommand="true" && onlyFetch="false"
+  [ "$arg" == "--help" ] || [ "$arg" == "-h" ] && showHelp=true
 done
 
 ## Tämä saattaa olla vielä väärin...
-[[ $runCustomCommand == "false" ]] && [[ $parallel == "false" ]] && [[ $onlyFetch == "false" ]] && [[ -z $doToAll ]] && [[ -n $1 ]] && \
-  echo "Unrecognized option!" && exit 1
+if [ $showHelp == true ]; then
+  echo "Note! All switches are written without the option symbol: -"
+  echo -e "\nValid switches are:"
+  echo "    parallel  OR  paral   Enables parallel running of git commands, but doesn't produce clear logs."
+  echo "    fetch                 For fetching (--all --prune), instead of also merging as with 'git pull'."
+  echo "    do                    For custom commands. Start without 'git' and Write all params in "quotes"."
+  exit
+elif [ $runCustomCommand == false ] && [ $parallel == false ] && [ $onlyFetch == false ] && [ -z "$doToAll" ] && [ -n "$1" ]; then
+  echo "Unrecognized option! Type -h or --help for help"
+  exit 1
+fi
 
-[[ $parallel == "true" ]] && [[ $runCustomCommand == "true" ]] && echo "Parallel running is not allowed with option 'do' because it \
+[ $parallel == true ] && [ $runCustomCommand == true ] && echo "Parallel running is not allowed with option 'do' because it \
 skews the logs and could do damage to all projects with unsafe parameters." && exit 2
 
 runGitCommand() {
-  [[ $parallel == "true" ]] && echo "Working on $shortPath." || echo -e "\nWorking on $shortPath. "
+  [ $parallel == true ] && echo "Working on $shortPath." || echo -e "\nWorking on $shortPath. "
 
-  if [[ $onlyFetch == "true" ]]; then
-    echo "Fetching all projects."
+  if [ $onlyFetch == true ]; then
     git -C "$fullPath" fetch --all --prune || exit 2
-  elif [[ $runCustomCommand == "true" ]]; then
+  elif [ $runCustomCommand == true ]; then
     echo "Running command: git -C $fullPath $doToAll"
     git -C "$fullPath" $doToAll || exit 2   ## Ei toimi jos "$doToAll", vain noin. Veikkaan että yrittäisi antaa silloin tarjota esim. git "branch --show-current" kun git taas osaa lukea vain git "branch" "--show-current". Mene ja tiedä!
   
   else
-  ## Run git pull
+  ## Run git pull (the default)
     local currentBranch
     currentBranch=$(git -C "$fullPath" branch --show-current)
-    git -C "$fullPath" checkout $branch && \
-    git -C "$fullPath" pull && \
-    git -C "$fullPath" checkout $currentBranch \
-    || exit 2
+    git -C "$fullPath" checkout $pullBranch && \
+      git -C "$fullPath" pull \
+      || exit 2
+
+    [ "$currentBranch" != "$pullBranch" ] && git -C "$fullPath" checkout $currentBranch || true  ## Tämä olikin erikoinen keissi... Ilman tuota viimeistä truea, kuvittelee ohjelman päättyneen virheeseen. Johtunee siitä että ajetaan  &  eli forkataan(?) ja test komento palauttaa false, joka sitten siirtyy viimeisenä suorituksena kys shellille(?).
   fi
 }
 
@@ -51,14 +62,14 @@ projects=$(find . -mindepth 1 -maxdepth 1 -type d)
 for path in $projects; do
   shortPath="${path:2}"
   fullPath="$(pwd)/$shortPath"
-  branch="develop"
+  pullBranch="develop"
   
   ## poikkeukset polkujen suhteen, esim eri oletushaara
-  [[ $shortPath == "cpi-token-test" ]] && branch="master" && echo -en "\nSetting default branch as 'master' for cpi-token-test."
-  [[ $shortPath == "EESSITestingki" ]] && echo -e "\nSkipping '$shortPath'." && continue ## intellij kansioni
-  [[ $shortPath == "yms" ]] && echo -e "\nSkipping '$shortPath'." && continue  ## muut kokeilut
+  [ $shortPath == "cpi-token-test" ] && pullBranch="master" && echo -en "\nSetting default branch as 'master' for cpi-token-test."
+  [ $shortPath == "EESSITestingki" ] && echo -e "\nSkipping '$shortPath'." && continue ## intellij kansioni
+  [ $shortPath == "yms" ] && echo -e "\nSkipping '$shortPath'." && continue  ## muut kokeilut
 
-  if [[ $parallel == "true" ]]; then
+  if [ $parallel == true ]; then
       runGitCommand &
     else
       runGitCommand
@@ -77,11 +88,11 @@ for p in $pids; do
     fi
 done
 
-if [[ $failures -gt 0 ]]; then
+if [ $failures -gt 0 ]; then
     echo "Out of all the projects, $failures failed. Exiting with error. Run without parallel processing to get clearer logs!"
     exit 1
   else
-    [[ $runCustomCommand == "true" ]] || [[ $onlyFetch ]] && echo -e "\nAll projects processed successfully!" || \
+    [ $runCustomCommand == true ] || [ $onlyFetch == true ] && echo -e "\nAll projects processed successfully!" || \
     echo -e "\nAll projects were pulled successfully!"  
     exit
 fi
