2f5b716f756e610bb8a982bbeb5f611f4e33e951
Juuso Välimaa <juuso.valimaa@kela.fi>
2023-05-17 19:17:26 +0300

    Absolute magic
    Ehkäpä parhaat skriptit mitä olen tähän asti kirjoittanut. Ainakin käytännöllisimmät. Pitäisi ottaa melkoisen suuri joukko epätavallisia olosuhteita huomioon ja olla intuitiivinen käyttää. Sisältää myös ihan ok ohjeistuksen, sanoisin.

--- omat/.config/omat/skriptit/f-f-f-find.sh
+++ omat/.config/omat/skriptit/f-f-f-find.sh
@@ -3,26 +3,31 @@ source "$(dirname "$0")/fail.sh"
 
 exactMatchesOnly=false  ## by default searches for names 'containing' search term
 ignoreCase=true
+X=""
+
+printHelp() {
+  echo "        f-f-f-find.sh - find for humans"
+  echo "Uses find to look for files by filename recursively. Cannot access files outside user's privileges."
+  echo 
+  echo 'Usage:'
+  echo '  f-f-f-find.sh [OPTION]... "arg1"        find a file with arg1'\''s content from current directory'
+  echo '  f-f-f-find.sh [OPTION]... "arg1" arg2   find a file with arg1'\''s content from arg2'\''s directory'
+  echo 
+  echo 'Options (must be spelled out before "arg1"):'
+  echo '  -i    make case significant, by default it'\''s ignored'
+  echo '  -e    remove implied wildcards *around* words resulting in exact matches only'
+  echo '  -X    sometimes useful; keeps prints taller than terminal height usually on'
+  echo '        screen (depending on whether lines occupy only one line - uses '\''less -X'\'')'
+  exit
+}
 
 ## Hjälp
 for arg in "$@"; do
-  if [ "$arg" == "--help" ] || [ "$arg" == "-h" ]; then
-    echo "        f-f-f-find.sh - find for humans"
-    echo "Uses find to look for files by filename recursively"
-    echo 
-    echo 'Usage:'
-    echo '  f-f-f-find.sh [OPTION]... "arg1"        find a file with arg1'\''s content from current directory'
-    echo '  f-f-f-find.sh [OPTION]... "arg1" arg2   find a file with arg1'\''s content from arg2'\''s directory'
-    echo 
-    echo 'Options (must be spelled out before "arg1"):'
-    echo '  -i    make case significant, by default it'\''s ignored'
-    echo '  -e    remove implied wildcards *around* words resulting in exact matches only'
-    exit
-  fi
+  [ "$arg" == "--help" ] || [ "$arg" == "-h" ] && printHelp
 done
 
 ## Optioiden käsittely
-while getopts "iex" OPTION; do
+while getopts "ieXh" OPTION; do
   case "$OPTION" in
     i)
       echo "        -- Case is significant --"
@@ -32,8 +37,11 @@ while getopts "iex" OPTION; do
       echo "        -- Exact matches only --"
       exactMatchesOnly=true
       ;;
-    x)
-      echo "  -- (DEBUG: ignoreCase=$ignoreCase, exactMatchesOnly=$exactMatchesOnly. Received params: $*) --"
+    X)
+      X="X"
+      ;;
+    h)
+      printHelp
       ;;
     *)
       ## Perään lisättävien argumenttien lisäksi Bash käyttää samaa OPTARG -muuttujaa myös virheellisille vivuille!
@@ -46,34 +54,68 @@ shift "$(($OPTIND -1))"
 
 [ $ignoreCase = true ] && name=iname || name=name
 
-## Hakulogiikka
+
+## Hakulogiikka (kutsutaan alempaa)
 findIt() {
   local term; term="$1"
   local path; path="$2"
 
+  pathStringLength="${#path}"
+  lastChar="${path:$((pathStringLength-1)):pathStringLength}"
+  if [ -d "$path" ] && [ $lastChar != "/" ]; then
+    echo "( Path is a directory, appended '/' to it:  '$path/' )"
+    path="${path}/"
+  fi
+
+  screenHeight=$(tput lines)
+
+  ## HUOM! Alla olevassa blokissa ikävästi toistuu sama find lause per haarauma... Se johtuu pitkälti näiden shellien
+  ## varsin epäluotettavasta tavasta käsitellä stringejä. On parempi pysyä järjissään, kuin kirjoittaa kauneinta koodia.
   if [ $exactMatchesOnly = false ]
     then
-      ## Kuin String.contains, etsii substringiä. Suodatetaan * koska less ja find hakujen erotessa eka ruutu ei tulostu.
-      ## TODO: Tämäkään toteutus ei miellytä, koska pitäisi myös tulostaa vain siinä tapauksessa että löytyy jotain... Muuten tulee
-      ## taas se sama valitus ruutu, että eipä mittää näy. Voe voe... Pitää jättää hautumaan!
-      if [[ "$term" == *"*"* ]];
-        then find "$path" -$name "*$term*" 2> /dev/null | less -FRX
-        else find "$path" -$name "*$term*" 2> /dev/null | less -FRX -Ip "$term"
-      fi                           
+      if [[ "$term" == *"*"* ]];  ## *1
+        then
+          find "$path" -$name "*$term*" 2> /dev/null | less -FR$X  ## *2
+        
+        else
+          outputHeight=$(find "$path" -$name "*$term*" 2> /dev/null | wc -l)
+          if [ $outputHeight -gt $screenHeight ]
+            then find "$path" -$name "*$term*" 2> /dev/null | less -FR$X -Ip "$term"
+            else find "$path" -$name "*$term*" 2> /dev/null  ## *3
+            echo "$outputHeight -gt $screenHeight"
+            echo "test $outputHeight -gt $screenHeight"
+          fi
+      fi
+    
     else
       if [[ "$term" == *"*"* ]];
-        then find "$path" -$name "$term" 2> /dev/null     | less -FRX
-        else find "$path" -$name "$term" 2> /dev/null     | less -FRX -p "$term"
+        then
+          find "$path" -$name "$term" 2> /dev/null | less -FR$X
+
+        else
+          outputHeight=$(find "$path" -$name "$term" 2> /dev/null | wc -l)
+          if [ $outputHeight -gt $screenHeight ]
+            then find "$path" -$name "$term" 2> /dev/null | less -FR$X -Ip "$term"
+            else find "$path" -$name "$term" 2> /dev/null
+          fi
       fi
   fi
 }
+## *1 Tällä suodatetaan * merkit pois koska less ja find hakujen erotessa eka ruutu ei tulostu. Toimii kuin "contains (substring)".
+## *2 Tämä on kompromissi. Monimutkaiset haut, joissa on * merkkejä eivät kuitenkaan tulostuisi samannäköisinä, joten valmis maalaus pois.
+## *3 Samaten kompromissi. Jos alle ruudun olevalle löydökselle ajettaisiin less maalaus, tulostuisi rumasti ~ merkkejä.
+
 
 noOfArgs=$#
-if [ $noOfArgs -eq 1 ]
-  then findIt "$1" "./"
-elif [ $noOfArgs -eq 2 ]
-  then
+if [ $noOfArgs -eq 1 ]; then
+    findIt "$1" "./"
+    exit 0
+
+elif [ $noOfArgs -eq 2 ]; then
+    path="$2"
     [ -d "$2" ] || fail "The path '$2' is not a valid directory." 2
     findIt "$1" "$2"
-else fail "Incorrect amount of arguments. Type -h for help. Note that options must be placed first.\nComplex queries are disabled, because 'find' is difficult to use. For example the order of arguments is significant and compounding." 2
+    exit 0
+else
+    fail "Incorrect amount of arguments. Type -h for help. Note that options must be placed first.\n(Complex queries are disabled, because find is difficult to use. For example the order of arguments is significant and compounding.)"
 fi
--- omat/.config/omat/skriptit/g-g-g-grep.sh
+++ omat/.config/omat/skriptit/g-g-g-grep.sh
@@ -1,42 +1,81 @@
 #!/bin/bash
 source "$(dirname "$0")/fail.sh"
 
+ignoreCase="--ignore-case"
+recursive="--dereference-recursive"
+## TODO: Jos aiot jättää tähän nämä optioiden käsittelyt (onhan tuo -X ihan kiva) Niin on pakko lisätä
+## oikea optioiden käsittely, koska tällä tyylillä ei voi yhdistää näitä optioita saman viivan alle!
+## TODO: Mieti tuoko nämä oikeaa lisäarvoa... Debuggaus turha ja tämä '[ ${path:0:1} != "-" ]' karmea!
 ## Yksinkertaistettu (uudelleenkirjoitettu) g-g-g-grep.sh
+printHelp() {
+  echo "        g-g-g-grep.sh - grep for humans"
+  echo "Uses grep to search for contents of files recursively. Cannot access files outside user's privileges."
+  echo
+  echo "Usage:"
+  echo '  g-g-g-grep.sh "arg1"            search for arg1'\''s content from current directory'
+  echo '  g-g-g-grep.sh "arg1" arg2       search for arg1'\''s content from arg2'\''s directory'
+  echo '  g-g-g-grep.sh "arg1" arg2 argN  search for arg1'\''s content from arg2'\''s directory with argN parameters given to grep'
+  echo 'Options (must be spelled out before "arg1"):'
+  echo " NOTE! HUOM! OBS! ATTENZION! Options given in beginning go to this script, options in the end go to grep (except -h which always prints this)!"
+  echo '  -i    turn "--ignore-case" off which is on by default - makes case significant'
+  echo '  -X    keeps printed text on screen (usually) - controls whether -X option is given to less'
+  echo '  -r    change "--dereference-recursive" into normal "--recursive", because it could help'
+  echo '  -d    turn all recursion off'
+  echo '  -x    display debug print'
+  exit
+}
+
 for arg in "$@"; do
-  if [ "$arg" == "-x" ]; then
-    echo "  -- (DEBUG: Received params as a whole: '$*') --"
-    isDebug=true
-    shift
-  fi
-  if [ "$arg" == "-X" ]; then
-    X="X"
-    shift
-  fi
-  if [ "$arg" == "--help" ] || [ "$arg" == "-h" ]; then
-    echo "        g-g-g-grep.sh - grep for humans"
-    echo "Uses grep to search for contents of files recursively"
-    echo
-    echo "Usage:"
-    echo '  g-g-g-grep.sh "arg1"            search for arg1'\''s content from current directory'
-    echo '  g-g-g-grep.sh "arg1" arg2       search for arg1'\''s content from arg2'\''s directory'
-    echo '  g-g-g-grep.sh "arg1" arg2 argN  search for arg1'\''s content from arg2'\''s directory with argN parameters given to grep'
-    echo 'Options (must be spelled out before "arg1"):'
-    echo '  -x    display debug print'
-    echo '  -X    if the -X option is to be given to grep which keeps printed text on screen'
-    exit
-  fi
+  [ "$arg" == "--help" ] || [ "$arg" == "-h" ] && printHelp
 done
 
-[ $isDebug == true ] && echo "  -- (Received params separately after shift: 1=$1, 2=$2, 3=$3, 4=$4, 5=$5, 6=$6, 7=$7, 8=$8) --"
+## Optioiden käsittely
+while getopts "irdXxh" OPTION; do
+  case "$OPTION" in
+    i)
+      echo "        -- Case is significant --"
+      ignoreCase=""
+      ;;
+    r)
+      recursive="--recursive"
+      ;;
+    d)
+      recursive=""
+      ;;
+    X)
+      X="X"
+      ;;
+    x)
+      echo "  -- (DEBUG: Received params as a whole at this moment: '$*') --"
+      isDebug=true
+      ;;
+    h)
+      printHelp
+      ;;
+    *)
+      ## Perään lisättävien argumenttien lisäksi Bash käyttää samaa OPTARG -muuttujaa myös virheellisille vivuille!
+      fail "Incorrect option '$OPTARG'. Type -h for help!"
+      ;;
+  esac
+done
+## getopts käytön jälkeen täytyy "nollata" argumenttien indeksi, että saadaan "tavalliset" argumentit mukaan
+shift "$(($OPTIND -1))"
 
+[ "$isDebug" == true ] && echo "  -- (DEBUG: Received params separately after shift: 1=$1, 2=$2, 3=$3, 4=$4, 5=$5, 6=$6, 7=$7, 8=$8) --"
+
+
+## Ohjelmalogiikka
 noOfArgs=$#
-
 if [ $noOfArgs -lt 1 ]; then
   fail 'At least one argument is needed'
+
 elif [ $noOfArgs -eq 1 ]; then
-  grep --fixed-strings --ignore-case --dereference-recursive --color=always "$1" ./* 2> /dev/null | less -FR$X
+  grep --fixed-strings $ignoreCase $recursive --color=always "$1" ./* 2> /dev/null | less -FR$X
+  exit 0
+
 elif [ $noOfArgs -gt 1 ]; then
   path="$2"
-  [ -d "$path" ] || fail "The path '$path' is not a valid directory." 2
-  grep --fixed-strings --ignore-case --dereference-recursive --color=always "$@" 2> /dev/null | less -FR$X
+  ! [ -d "$path" ] && [ ${path:0:1} != "-" ] && fail "The path '$path' is not a valid directory." 2
+  grep --fixed-strings $ignoreCase $recursive --color=always "$@" 2> /dev/null | less -FR$X
+  exit 0
 fi
