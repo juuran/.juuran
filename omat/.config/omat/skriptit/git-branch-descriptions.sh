fc86d9524dfd0fbbf6d0a922cb6cf427f2037a39
Juuso Välimaa <juuso.valimaa@kela.fi>
2023-10-12 10:23:02 +0300

    Lisää/parempia aliaksia. Isompi haaran nimen leveys git-branch-descriptionsiin (koska KELA).

--- omat/.config/omat/skriptit/git-branch-descriptions.sh
+++ omat/.config/omat/skriptit/git-branch-descriptions.sh
@@ -14,7 +14,7 @@ NO_COLOR='\033[0m'
 GREEN=$(tput setaf 76)
 
 PAD="                                                                             "  ## (parempi liikaa kuin liian vähän)
-BRANCH_MAX_LENGTH=60
+BRANCH_MAX_LENGTH=90
 BRANCH_LENGTH=$(( $(tput cols) / 3 ))
 [ $BRANCH_LENGTH -gt $BRANCH_MAX_LENGTH ] && BRANCH_LENGTH=$BRANCH_MAX_LENGTH
 ## echo "tput cols=$(tput cols)  BRANCH_LENGTH=$BRANCH_LENGTH "  ## debug
@@ -27,11 +27,11 @@ for branch in $BRANCHES
         truncated_branch_name="${padded_branch:0:BRANCH_LENGTH}"
 
         if [ ${#description} -gt $DESCR_LENGTH ]
-            then truncated_descr="${description:0:$((DESCR_LENGTH - 3))}..."
+            then truncated_descr="${description:0:$((DESCR_LENGTH - 4))}..."
             else truncated_descr="$description"
         fi
 
-        formatted_line="$truncated_branch_name $truncated_descr"
+        formatted_line="$truncated_branch_name  $truncated_descr"
         if [ $branch = $CURRENT ]
             then echo -e "* ${GREEN}${formatted_line}${NO_COLOR}"
             else echo -e "  $formatted_line"
