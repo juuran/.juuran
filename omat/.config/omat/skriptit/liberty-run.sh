83e44d0e331ed69003e7471dfed334d354e0615f
Juuso Välimaa <juuso.valimaa@kela.fi>
2024-03-12 14:37:58 +0200

    .aliases siistitty, lisätty liberty-run omana skriptinään jolla vivut
    juuson maailma on taas pikkuisen parempi paikka

---
+++ omat/.config/omat/skriptit/liberty-run.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+source "$(dirname "$0")/fail.sh"
+
+handleSignals() {
+    echo -e "\nkeskeytetään open libertyn käynnistys..."
+    exit 1
+}
+
+trap 'handleSignals' SIGINT
+trap 'handleSignals' SIGQUIT
+
+if ! [ -f "./pom.xml" ] || ! [ -d "./${PWD##*/}-ear" ]; then
+    fail "No pom.xml or -ear subdirectory found"
+fi
+[ -n "$1" ] && [[ "$1" != *-* ]] && echo "(argumentti ohitettu, koska skripti ei käytä niitä)"
+
+while getopts "sc" OPTION; do
+    case "$OPTION" in
+    s)
+        isSkipTests=true
+        ;;
+    c)
+        isCleanBuild=true
+        ;;
+    *)
+        fail "vain seuraavia vipuja tuetaan: \n-s (skippaa testit)\n-c (clean build)"
+        ;;
+    esac
+done
+shift "$(($OPTIND -1))"
+
+if [ "$isCleanBuild" == true ]; then
+    if [ "$isSkipTests" == true ]
+        then echo "tehdään puhdas build ilman testejä..." && mvn clean install -DskipTests
+        else echo "tehdään puhdas build..." && mvn clean install
+    fi
+else
+    echo -e "käynnistetään open liberty...\n" && sleep 0.5
+fi
+
+mvn -pl "${PWD##*/}-ear" -e -P local liberty:run
