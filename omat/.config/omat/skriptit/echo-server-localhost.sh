def064f30dd63e73e4ceb2a97792ab5c6fbab183
Juuso Välimaa <juuso.valimaa@kela.fi>
2024-04-17 11:53:43 +0300

    lisätty köpöinen localhost echo serveri

---
+++ omat/.config/omat/skriptit/echo-server-localhost.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+source "$(dirname "$0")/fail.sh"
+
+## Tukee vain yhtä porttia
+if [ -n "$1" ];
+    then [ "$1" -eq $(($1)) ] || fail "annetun portin pitää olla numero!"
+         PORT=$1
+    else PORT=6996
+fi
+
+echo -e "aloitetaan echo server paikallisesti portissa $PORT..."
+echo -e "(mikäli portti on käytössä ja tulee virhe, paina ENTER lopettaaksesi)\n"
+
+# Start the server and listen for incoming connections
+while true; do
+  (echo -ne "HTTP/1.1 200 OK\r\n"; cat) \
+    | nc -l -p $PORT \
+    || fail "porttinumero ei ole sallittu."
+done
