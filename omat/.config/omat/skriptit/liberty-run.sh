aee2856b89f25044271aaf49c0a82b6221632637
Juuso Välimaa <juuso.valimaa@kela.fi>
2024-09-25 13:14:32 +0300

    niin se vaan tämäkin pikkuskripti lähentelee "oikeaa" skriptiä
    - (selvästi nuo meikäläisen kehittämät "best practiset" on
       ihan fiksuja, kun kaikki skriptit alkaa kuin itsestään
       niitä lähentyä... eihän sitä muuten löydä tietoa!)

--- omat/.config/omat/skriptit/liberty-run.sh
+++ omat/.config/omat/skriptit/liberty-run.sh
@@ -1,76 +1,102 @@
 #!/bin/bash
 source "$(dirname "$0")/fail.sh"
 
+printHelp() {
+    echo "vain seuraavia vipuja tuetaan:"
+    echo "-s(kippaa testit)"
+    echo "-c(lean build)"
+    echo "-n(o liberty will be run)"
+    echo "-d(ev)"
+}
+
 handleSignals() {
     echo -e "\n$(basename "$0") lopetettu"
     exit 1
 }
 
-trap 'handleSignals' SIGINT
-trap 'handleSignals' SIGQUIT
-
-## vähän ruma fixi, kun aiemmassa oli hupsu oletus pää ja ear polun yhdennimisyydestä
-# "${PWD##*/}-ear"
-earPaths="$(ls -d */ | grep "\-ear" | cut -d / -f 1)"
-
-earPathFound=false
-for path in $earPaths; do
-    if [ -d "$path" ]; then
-        earPathFound=true
-        earPath="$path"
-    fi
+## optioiden käsittely
+for arg in "$@"; do
+  [ "$arg" == "--help" ] || [ "$arg" == "-h" ] && printHelp && exit
 done
 
-if ! [ -f "./pom.xml" ] || [ $earPathFound == false ]; then
-    fail "Both pom.xml and -ear subdirectories are needed, condition not met"
-fi
-
-echo "heuristisesti päätelty, että earin polku on: $earPath"
-
-[ -n "$1" ] && [[ "$1" != *-* ]] && echo "(argumentti ohitettu, koska skripti ei käytä niitä)"
-
-## defaultit
 isSkipTests=false
 isBuild=false
 isDontRunLiberty=false
-while getopts "sbcn" OPTION; do
+mode="run"
+clean=""
+puhdas=""
+while getopts "sbcdn" OPTION; do
     case "$OPTION" in
     s)
         isSkipTests=true
         ;;
     b)
         isBuild=true
-        clean=""
-        puhdas=""
         ;;
     c)
         isBuild=true
         clean="clean"
         puhdas="puhdas "
         ;;
+    d)
+        mode="dev"
+        ;;
     n)
         isDontRunLiberty=true
         ;;
     *)
-        fail "vain seuraavia vipuja tuetaan: \n-s (skippaa testit)\n-c (clean build)\n-n (no liberty be run)"
+        printHelp
+        fail "ei tuettu vipu"
         ;;
     esac
 done
 shift "$(($OPTIND -1))"
 
-if [ "$isBuild" == true ]; then
-    if [ "$isSkipTests" == true ]
-        then
-            echo "tehdään ${puhdas}buildi ilman testejä..."
-            mvn -e $clean install -DskipTests || fail "\nmaven build epäonnistui!"
-        else 
-            echo "tehdään ${puhdas}buildi..."
-            mvn -e $clean install || fail "\nmaven build epäonnistui!"
+
+main() {
+    trap 'handleSignals' SIGINT
+    trap 'handleSignals' SIGQUIT
+
+    ## vähän ruma fixi, kun aiemmassa oli hupsu oletus pää ja ear polun yhdennimisyydestä
+    # "${PWD##*/}-ear"
+    earPaths="$(ls -d */ | grep "\-ear" | cut -d / -f 1)"
+
+    earPathFound=false
+    for path in $earPaths; do
+        if [ -d "$path" ]; then
+            earPathFound=true
+            earPath="$path"
+        fi
+    done
+
+    if ! [ -f "./pom.xml" ] || [ $earPathFound == false ]; then
+        fail "Both pom.xml and -ear subdirectories are needed, condition not met"
     fi
-    echo -e "maven build onnistui!\n"
-fi
 
-[ "$isDontRunLiberty" == true ] && exit 0
+    echo "päätelty, että earin polku on: $earPath"
 
-echo -e "käynnistetään open liberty...\n" && sleep 0.5
-mvn -pl "$earPath" -e -P local liberty:run
+    [ -n "$1" ] && [[ "$1" != *-* ]] && echo "(argumentti ohitettu, koska skripti ei käytä niitä)"
+
+    if [ "$isBuild" == true ]; then
+        if [ "$isSkipTests" == true ]
+            then
+                echo "tehdään ${puhdas}buildi ilman testejä..."
+                echo "(    suoritetaan:   mvn -e $clean install -DskipTests    )"
+                mvn -e $clean install -DskipTests || fail "\nmaven build epäonnistui!"
+            else 
+                echo "tehdään ${puhdas}buildi..."
+                echo "(    suoritetaan:   mvn -e $clean install    )"
+                mvn -e $clean install || fail "\nmaven build epäonnistui!"
+        fi
+        echo -e "maven build onnistui!"
+    fi
+
+    [ "$isDontRunLiberty" == true ] && exit 0
+
+    echo -e "\nkäynnistetään open liberty..."
+    echo "(    suoritetaan:   mvn -pl "$earPath" -e -P local liberty:$mode    )"
+    sleep 0.5
+    mvn -pl "$earPath" -e -P local liberty:$mode
+}
+
+main "$@"  ## antaa vain "jäljellä olevat" argumentit eli optioissa käsitellyt jo poistettu
