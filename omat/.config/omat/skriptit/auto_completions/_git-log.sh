2aecf24494082080d0741700dd52efbc0d1f7617
Juuso Välimaa <juuso.valimaa@kela.fi>
2024-11-13 11:04:42 +0200

    useampi näyttömoodi git-log skriptiin

--- omat/.config/omat/skriptit/auto_completions/_git-log.sh
+++ omat/.config/omat/skriptit/auto_completions/_git-log.sh
@@ -8,7 +8,7 @@ function _anna_git_haarat() {
     _describe -t output 'gitin haarat' haarat
 }
 
-viewModet='( 1\:"väljä ja selkeä tyyli (default)"  2\:"selkeä yksirivinen tyyli" 3\:"vanha yksirivinen tyyli, joka näyttää kaikki \"ref\":it" )'
+viewModet='( 1\:"väljä ja selkeä tyyli (oletus, topologian mukaan)" 2\:"väljä ja selkeä tyyli (ajan mukaan)"  3\:"selkeä yksirivinen tyyli (topologian mukaan)" 4\:"selkeä yksirivinen tyyli (ajan mukaan)" 5\:"legacy yksirivinen tyyli, joka näyttää \"ref\":it lyhentämättöminä" )'
 
 _arguments -s \
     '1:haarat:_anna_git_haarat' \
--- omat/.config/omat/skriptit/git-log.sh
+++ omat/.config/omat/skriptit/git-log.sh
@@ -6,18 +6,20 @@ viewMode=1
 branch=""
 noOfArgs=$#
 cWidth=3
+opts="--decorate --color"
+example="Example of a correct way:\n    git-log <branch (opt)> <viewmode 1-5 (opt)>"
 
 if [ "$noOfArgs" -gt 2 ]; then
-    fail "Erroneous amount of arguments. Required 0-2 arguments. Example of a correct way:\n    git-log <branch (opt)> <viewmode 1-3 (opt)>"
+    fail "Erroneous amount of arguments. Required 0-2 arguments. $example"
 fi
 
 for arg in "$@"; do
     [ -z "$arg" ] && continue
     
-    if [[ "$arg" =~ ^[1-3]{1}$ ]]; then
+    if [[ "$arg" =~ ^[1-5]{1}$ ]]; then
         viewMode="$arg"
-    elif [[ "$arg" =~ ^[4-9]+$ ]] || [[ "$arg" =~ ^[0]+[0-9]*$ ]] || [[ "$arg" =~ ^[1-3]+[0-9]+$ ]]; then
-        fail "Only numbers 1-3 are allowed for viewmode."
+    elif [[ "$arg" =~ ^[6-9]+$ ]] || [[ "$arg" =~ ^[0]+[0-9]*$ ]] || [[ "$arg" =~ ^[1-5]+[0-9]+$ ]]; then
+        fail "Only numbers 1-5 are allowed for viewmode. $example"
     else
         branch="$arg"
     fi
@@ -27,9 +29,13 @@ if [ -z "$branch" ] || [ "$branch" == "--show-current" ]; then branch="$(git bra
 fi
 
 if      [ "$viewMode" == 1 ]; then  ## (oletus)
-    git log --graph --date='format:%d.%m.%Y-- %H:%M:%S' --decorate=short --color --pretty=format:'^%C(bold dim white)%ad%C(reset)^%C(bold dim cyan)%<(19,trunc)%an%C(reset)^%C(bold cyan)%h%C(reset)^%C(auto)%D%C(reset)%n^%C(dim white)%<(19,trunc)%ar%C(reset)^%C(dim cyan)%<(19,trunc)%ae%C(reset)^%C(bold white)Commit:%C(reset)^%C(white)%s%C(reset)%n' "$branch" | column -t -s ^ -c $cWidth | less
+    git log --graph --topo-order --date='format:%d.%m.%Y-- %H:%M:%S' $opts --pretty=format:'^%C(bold dim white)%ad%C(reset)^%C(bold dim cyan)%<(19,trunc)%an%C(reset)^%C(bold cyan)%h%C(reset)^%C(auto)%D%C(reset)%n^%C(dim white)%<(19,trunc)%ar%C(reset)^%C(dim cyan)%<(19,trunc)%ae%C(reset)^%C(bold white)Commit:%C(reset)^%C(white)%s%C(reset)%n' "$branch" | column -t -s ^ -c $cWidth | less
 elif    [ "$viewMode" == 2 ]; then
-    git log --graph --abbrev-commit --decorate --color --format=format:'^%C(bold blue)%h%C(reset)^%C(bold green)%<(15,trunc)%ar%C(reset)^%C(dim white)%<(22,trunc)%an^%C(auto)%<(28,trunc)%d^%C(white)%s%C(reset)' "$branch" | column -t -s ^ -c $cWidth | less
+    git log --graph --date-order --date='format:%d.%m.%Y-- %H:%M:%S' $opts --pretty=format:'^%C(bold dim white)%ad%C(reset)^%C(bold dim cyan)%<(19,trunc)%an%C(reset)^%C(bold cyan)%h%C(reset)^%C(auto)%D%C(reset)%n^%C(dim white)%<(19,trunc)%ar%C(reset)^%C(dim cyan)%<(19,trunc)%ae%C(reset)^%C(bold white)Commit:%C(reset)^%C(white)%s%C(reset)%n' "$branch" | column -t -s ^ -c $cWidth | less
 elif    [ "$viewMode" == 3 ]; then
-    git log --graph --abbrev-commit --decorate --color --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' "$branch" | less
+    git log --graph --topo-order --abbrev-commit $opts --format=format:'^%C(bold blue)%h%C(reset)^%C(bold green)%<(15,trunc)%ar%C(reset)^%C(dim white)%<(14,trunc)%an^%C(auto)%<(28,trunc)%d^%C(white)%s%C(reset)' "$branch" | column -t -s ^ -c $cWidth | less
+elif    [ "$viewMode" == 4 ]; then
+    git log --graph --date-order --abbrev-commit $opts --format=format:'^%C(bold blue)%h%C(reset)^%C(bold green)%<(15,trunc)%ar%C(reset)^%C(dim white)%<(14,trunc)%an^%C(auto)%<(28,trunc)%d^%C(white)%s%C(reset)' "$branch" | column -t -s ^ -c $cWidth | less
+elif    [ "$viewMode" == 5 ]; then
+    git log --graph --topo-order --abbrev-commit $opts --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' "$branch" | less
 fi
