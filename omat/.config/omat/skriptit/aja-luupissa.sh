bf5361eb50df0fbd12c1a457f846b0a8df326646
Juuso Välimaa <juuso.h.valimaa@gmail.com>
2025-11-05 13:03:16 +0200

    mainiota kikkailua ala aja-luupissa.sh juuso

---
+++ omat/.config/omat/skriptit/aja-luupissa.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+source "$SKRIPTIT_POLKU/fail.sh"
+
+handleSigInt() {
+    [ $RUNNING = false ] && fail "saatu pyyntö keskeyttää, lopetettu onnistuneesti" 0
+    echo -n "keskeytetään ajossa oleva kutsu... "
+    RUNNING=false
+}
+
+RUNNING=true
+KOMENTO="$*"
+
+ajaLuupissa() {
+
+    [ -z "$KOMENTO" ] && fail 'argumentti vaaditaan, esim. "echo moi && sleep 1"'
+    trap 'handleSigInt' SIGINT
+    trap 'handleSigQuit' SIGQUIT
+
+    echo "-- yksi SIGINT        keskeyttää ajossa olevan komennon --"
+    echo "-- kaksi SIGINTIÄ     lopettaa tämän skriptin"
+    sleep 1
+    echo -e "\naloitetaan ajamaan luupissa: $KOMENTO\n"
+
+    while true; do
+        while [ $RUNNING = true ]; do
+            $KOMENTO
+        done
+        echo -e "keskeytetty, mutta kännistetään pian uudelleen. \n(lopeta serveri toisella SIGINTILLÄ [ctrl + c])\n"
+        sleep 1
+
+        RUNNING=true
+    done
+}
+
+ajaLuupissa "$@"
