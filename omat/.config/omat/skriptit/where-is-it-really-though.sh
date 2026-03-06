157766725456c3557fb1d29fcaf05eb90ebe2daf
Välimaan Juuso <juuran@gmail.com>
2026-03-06 16:56:08 +0200

    nätistetty where-is-it skripti
    - haetaan se nyt suoraan "muistiin", että olisi tietoinen
      myös itse määritellyistä funktioista ja aliaksista paremmin
        - siistimmän näköinen ja parempaa tietoa
    - näyttää nyt myös värit ja onpa hiano!
    - aliakset nyt myös kaikille itse määritellyille (aliasmaisille)
      funktioille

--- omat/.config/omat/skriptit/where-is-it-really-though.sh
+++ omat/.config/omat/skriptit/where-is-it-really-though.sh
@@ -1,30 +1,19 @@
 #!/bin/zsh
 
-arg="$1"
-alku="[  "
-lopu=" ]"
+function really_though_where() {
+    local arg alku lopu noColor ehree
+    arg="$1"
+    alku="[ "
+    lopu=" ]"
+    
+    noColor='\033[0;37m'
+    ehree='\033[2;92m'
 
-command -v whereis 1> /dev/null 2> /dev/null \
-    && echo -e "$alku whereis $lopu" \
-    && whereis $arg
-    echo -e "\n"
-
-command -v where 1> /dev/null 2> /dev/null \
-    && echo -e "$alku where $lopu" \
-    && where $arg
-    echo -e "\n"
-
-command -v whence 1> /dev/null 2> /dev/null \
-    && echo -e "$alku whence $lopu" \
-    && whence $arg
-    echo -e "\n"
-
-command -v which 1> /dev/null 2> /dev/null \
-    && echo -e "$alku which $lopu" \
-    && which $arg
-    echo -e "\n"
-
-command -v which-command 1> /dev/null 2> /dev/null \
-    && echo -e "$alku which-command $lopu" \
-    && which-command $arg
-    echo -e "\n"
+    for cmd in whatis alias whereis where whence which which-command; do
+        if command -v $cmd 1> /dev/null 2> /dev/null; then
+            echo -e "${ehree}$alku $cmd $lopu${noColor}"
+            $cmd $arg
+            echo -e ""
+        fi
+    done
+}
