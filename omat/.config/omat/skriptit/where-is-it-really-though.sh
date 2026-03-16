5307035f1799812bfd7a1ec392182e1269bca8a9
Välimaan Juuso <juuran@gmail.com>
2026-03-16 13:45:02 +0200

    where-really selkeämpi ja toimivampi

--- omat/.config/omat/skriptit/where-is-it-really-though.sh
+++ omat/.config/omat/skriptit/where-is-it-really-though.sh
@@ -1,19 +1,16 @@
 #!/bin/zsh
 
 function really_though_where() {
-    local arg alku lopu noColor ehree
+    local arg vihrea noColor
     arg="$1"
-    alku="[ "
-    lopu=" ]"
-    
+    vihrea='\033[0;32m'
     noColor='\033[0;37m'
-    ehree='\033[2;92m'
 
-    for cmd in whatis alias whereis where whence which which-command; do
-        if command -v $cmd 1> /dev/null 2> /dev/null; then
-            echo -e "${ehree}$alku $cmd $lopu${noColor}"
-            $cmd $arg
-            echo -e ""
-        fi
+    for cmd in whatis alias whereis where whence which; do
+        command -v $cmd 1> /dev/null 2> /dev/null || continue
+
+        echo -e "${vihrea}[ $cmd ]${noColor}"
+        $cmd $arg
+        echo ""
     done
 }
